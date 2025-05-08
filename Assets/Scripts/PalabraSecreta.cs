3002569a5eb4c7b78d59b017c9b0f7d30706f884
MagaJL <48716048@est.ort.edu.ar>
2025-05-08 09:50:27 -0300

    Palabra secreta terminado

---
+++ Assets/Scripts/PalabraSecreta.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PalabraSecreta : MonoBehaviour
+{
+    public string palabraSecreta;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (palabraSecreta == "domingo")
+        {
+            Debug.Log("Has adivinado la palabra.");
+        }
+        else
+        {
+            Debug.Log("Palabra incorrecta");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
