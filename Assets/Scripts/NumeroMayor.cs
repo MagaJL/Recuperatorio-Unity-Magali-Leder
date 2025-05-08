d4105cb99b4ca033baef14a021458a6b9e419507
MagaJL <48716048@est.ort.edu.ar>
2025-05-08 09:46:14 -0300

    Numero Mayor terminado

--- Assets/Scripts/NumeroMayor.cs
+++ Assets/Scripts/NumeroMayor.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class NumeroMayor : MonoBehaviour
 {
+    public int num1;
+    public int num2;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (num1 > num2)
+        {
+            Debug.Log("El mayor de los dos números ingresados es: " + num1);
+        }
+        else if (num1 < num2)
+        {
+            Debug.Log("El mayor de los dos números ingresados es: " + num2);
+        }
+        else
+        {
+            Debug.Log("Los números ingresados son iguales.");
+        }
     }
 
     // Update is called once per frame
